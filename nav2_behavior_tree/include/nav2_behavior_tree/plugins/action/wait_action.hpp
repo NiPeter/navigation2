5be52c439f32506617e8cd6bbab471f611f08d5c
Piotr Nikiel <pnikiel@ess.etisoft.pl>
2021-10-05 12:56:33 +0200

    Adds WaitForNewGoal action

--- nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/wait_action.hpp
+++ nav2_behavior_tree/include/nav2_behavior_tree/plugins/action/wait_action.hpp
@@ -17,17 +17,18 @@
 
 #include <string>
 
+#include "behaviortree_cpp_v3/condition_node.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav2_behavior_tree/bt_action_node.hpp"
 #include "nav2_msgs/action/wait.hpp"
 
-namespace nav2_behavior_tree
-{
+namespace nav2_behavior_tree {
 
 /**
- * @brief A nav2_behavior_tree::BtActionNode class that wraps nav2_msgs::action::Wait
+ * @brief A nav2_behavior_tree::BtActionNode class that wraps
+ * nav2_msgs::action::Wait
  */
-class WaitAction : public BtActionNode<nav2_msgs::action::Wait>
-{
+class WaitAction : public BtActionNode<nav2_msgs::action::Wait> {
 public:
   /**
    * @brief A constructor for nav2_behavior_tree::WaitAction
@@ -35,10 +36,8 @@ public:
    * @param action_name Action name this node creates a client for
    * @param conf BT node configuration
    */
-  WaitAction(
-    const std::string & xml_tag_name,
-    const std::string & action_name,
-    const BT::NodeConfiguration & conf);
+  WaitAction(const std::string &xml_tag_name, const std::string &action_name,
+             const BT::NodeConfiguration &conf);
 
   /**
    * @brief Function to perform some user-defined operation on tick
@@ -49,15 +48,47 @@ public:
    * @brief Creates list of BT ports
    * @return BT::PortsList Containing basic ports along with node-specific ports
    */
-  static BT::PortsList providedPorts()
-  {
+  static BT::PortsList providedPorts() {
     return providedBasicPorts(
-      {
-        BT::InputPort<int>("wait_duration", 1, "Wait time")
-      });
+        {BT::InputPort<int>("wait_duration", 1, "Wait time")});
   }
 };
 
-}  // namespace nav2_behavior_tree
+/**
+ * @brief A BT::SyncActionNode that returns SUCCESS when goal is
+ * updated on the blackboard and RUNNING otherwise
+ */
+class WaitForNewGoal : public BT::ActionNodeBase {
+public:
+  /**
+   * @brief A constructor for nav2_behavior_tree::WaitForNewGoal
+   * @param xml_tag_name Name for the XML tag for this node
+   * @param conf BT node configuration
+   */
+  WaitForNewGoal(const std::string &xml_tag_name,
+                 const BT::NodeConfiguration &conf);
 
-#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__WAIT_ACTION_HPP_
+  WaitForNewGoal() = delete;
+
+  /**
+   * @brief The main override required by a BT action
+   * @return BT::NodeStatus Status of tick execution
+   */
+  BT::NodeStatus tick() override;
+
+  void halt() override { setStatus(BT::NodeStatus::IDLE); }
+
+  /**
+   * @brief Creates list of BT ports
+   * @return BT::PortsList Containing node-specific ports
+   */
+  static BT::PortsList providedPorts() { return {}; }
+
+private:
+  geometry_msgs::msg::PoseStamped goal_;
+  std::vector<geometry_msgs::msg::PoseStamped> goals_;
+};
+
+} // namespace nav2_behavior_tree
+
+#endif // NAV2_BEHAVIOR_TREE__PLUGINS__ACTION__WAIT_ACTION_HPP_
